56f133845501d4ae368597e5495fb18e12a459d3
shchegrikovich <shchegrikovich@gmail.com>
2016-09-14 19:21:11 +0300

    Added create functionality

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -1,24 +1,40 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using Db;
 using Microsoft.AspNetCore.Mvc;
+using Model;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private BloggingContext _context;
+        private readonly BloggingContext _context;
 
         public HomeController(BloggingContext context)
         {
             _context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var blogs = _context.Blogs.ToArray();
-            return View(blogs);
-        }        
+            return View("Index", blogs);
+        }
 
+        [HttpPost]
+        [Route("/")]
+        public async Task<IActionResult> CreateBlog(string url)
+        {
+            Blog b = new Blog();
+            b.Url = url;
+            _context.Blogs.Add(b);
+            await _context.SaveChangesAsync();
+
+            return Index();
+        }
+
+        [HttpGet]
         public IActionResult Error()
         {
             return View();
